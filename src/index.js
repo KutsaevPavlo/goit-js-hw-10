628b850f45659370354cca1151a99c66435335df
KutsaevPavlo <p1ul@ukr.net>
2023-01-28 23:54:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import API from "./fetchCountries.js"
+import { functions } from 'lodash';
 const DEBOUNCE_DELAY = 300;
 const _ = require('lodash');
 
@@ -18,13 +19,29 @@ function onInput(e){
     const form = e.target;
     // console.dir(form);
     const inputValue = form.value;
-    console.log(inputValue);
-    API.fetchCountries(inputValue).then(console.log);
-};
+    // console.log(inputValue);
+    API.fetchCountries(inputValue).then((data) => {
+        console.log(data);
+    });
+    };
 
+
+    function createMarkup({name.official, capital, population, flags, languages}){
+        return
+        `
+        <div class="countrie-card"
+        flags.svg
+        `
+    }
 
 
 // _.debounce(() => {
 //     console.log("Scroll handler call after 300ms pause");
 //   }, 300)
+
+// API.fetchCountries(inputValue).then(({data}) => {
+//     if(data.length === 0) throw new 
+//     Error("No data");
+//     console.log(0);
+// });
 
