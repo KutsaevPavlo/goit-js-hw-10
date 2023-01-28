1e1ae23fdcfb5167340287bc83ae696004e7f7cb
KutsaevPavlo <p1ul@ukr.net>
2023-01-28 23:20:38 +0200

    update lodash

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,25 @@
 //export
 
-const data = fetch("https://restcountries.com/v3.1/name/ukraine");
+const ENDPOINT = "https://restcountries.com/v3.1/name"
 
-console.log('123');
+function fetchCountries(name){
+return fetch(`${ENDPOINT}/${name}?fields=name.official,capital,population,flags,languages`)
+.then((res) => res.json())
+};
 
-// function fetchCountries(name){
+export default { fetchCountries };
 
-// };
 
-// https://restcountries.com/v3.1/name/{name}
+
+// https://restcountries.com/v2/{service}?fields={field},{field},{field}
+
+// https://restcountries.com/v2/all?fields=name,capital,currencies
+
+
+// Тобі потрібні тільки наступні властивості:
+
+// name.official - повна назва країни
+// capital - столиця
+// population - населення
+// flags.svg - посилання на зображення прапора
+// languages - масив мов
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import API from "./fetchCountries.js"
 const DEBOUNCE_DELAY = 300;
+const _ = require('lodash');
+
+API.fetchCountries("ukraine").then(console.log);
+
+const form = document.getElementById("search-box");
+
+form.addEventListener("input", _.debounce(onInput, DEBOUNCE_DELAY));
+// form.addEventListener("input", onInput);
+
+function onInput(e){
+    e.preventDefault();
+  
+    // const form = e.currentTarget;
+    const form = e.target;
+    // console.dir(form);
+    const inputValue = form.value;
+    console.log(inputValue);
+    API.fetchCountries(inputValue).then(console.log);
+};
 
 
-fetch("https://restcountries.com/v3.1/name/ukraine")
 
-.then((res) => res.json())
-.then((data) => console.log(data));
+// _.debounce(() => {
+//     console.log("Scroll handler call after 300ms pause");
+//   }, 300)
 
